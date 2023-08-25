15587e0e7bd0183d689bbae984b1d87ecb7293ac
Tarek Zeyad <tarekzeyad31@yahoo.com>
2023-08-26 01:13:56 +0300

    Finalized the editing after the placing order

--- Model/Resolver/SetOrderComment.php
+++ Model/Resolver/SetOrderComment.php
@@ -2,31 +2,48 @@
 namespace Vendor\OrderComment\Model\Resolver;
 
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Sales\Model\OrderFactory;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Vendor\OrderComment\Model\OrderCommentManagement;
 
 class SetOrderComment implements ResolverInterface
 {
-protected $orderCommentManagement;
+    /**
+     * @var OrderFactory
+     */
+    protected $orderFactory;
 
-public function __construct(OrderCommentManagement $orderCommentManagement)
-{
-$this->orderCommentManagement = $orderCommentManagement;
-}
+    /**
+     * @param OrderFactory $orderFactory
+     */
+    public function __construct(
+        OrderFactory $orderFactory
+    ) {
+        $this->orderFactory = $orderFactory;
+    }
 
-public function resolve(
-Field $field,
-$context,
-ResolveInfo $info,
-array $value = null,
-array $args = null
-) {
-$this->orderCommentManagement->setComment($args['cart_id'], $args['comment']);
-return [
-'status' => true,
-'message' => 'Comment added successfully.'
-];
-}
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $orderId = $args['order_id'];
+        $comment = $args['comment'];
+
+        // Load the order using increment id
+        $order = $this->orderFactory->create()->loadByIncrementId($orderId);
+        if (!$order->getId()) {
+            throw new LocalizedException(__("Order with ID %1 not found", $orderId));
+        }
+
+        // Set the comment
+        $order->setData('customer_comment', $comment);
+        $order->save();
+
+        return [
+            'order_id' => (int) $orderId,
+            'comment' => $comment
+        ];
+    }
 }
